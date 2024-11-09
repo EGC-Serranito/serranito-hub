f84f8b7f3644f99cd09acda78730dabd0ce19e4a
Dangalcan <megamagolas@gmail.com>
2024-11-09 12:38:56 +0100

    test: add app context to dataset tests in order to be able to run them without running the whole module test

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -5,6 +5,7 @@ from app.modules.dataset.services import DataSetService, calculate_checksum_and_
 from app.modules.dataset.models import DataSet, DSMetaData
 from app.modules.auth.models import User
 import os
+from app import create_app
 
 
 @pytest.fixture
@@ -24,22 +25,26 @@ def doi_mapping_service():
 
 @pytest.fixture
 def mock_user():
-    user = MagicMock(spec=User)
-    user.id = 1
-    user.temp_folder.return_value = "/mock/temp/folder"
-    user.profile.surname = "Doe"
-    user.profile.name = "John"
-    user.profile.affiliation = "Test University"
-    user.profile.orcid = "0000-0001-2345-6789"
-    return user
+    app = create_app()
+    with app.app_context():
+        user = MagicMock(spec=User)
+        user.id = 1
+        user.temp_folder.return_value = "/mock/temp/folder"
+        user.profile.surname = "Doe"
+        user.profile.name = "John"
+        user.profile.affiliation = "Test University"
+        user.profile.orcid = "0000-0001-2345-6789"
+        return user
 
 
 @pytest.fixture
 def mock_dataset():
-    dataset = MagicMock(spec=DataSet)
-    dataset.id = 1
-    dataset.feature_models = []
-    return dataset
+    app = create_app()
+    with app.app_context():
+        dataset = MagicMock(spec=DataSet)
+        dataset.id = 1
+        dataset.feature_models = []
+        return dataset
 
 
 def test_move_feature_models(dataset_service, mock_user, mock_dataset):
@@ -68,44 +73,49 @@ def test_create_from_form(dataset_service, mock_user):
         {"name": "Author One", "affiliation": "University", "orcid": "0000-0002-1234-5678"}
     ]
     form.feature_models = []
+    app = create_app()
+    with app.app_context():
+        with patch.object(dataset_service.dsmetadata_repository, 'create') as mock_create_dsmetadata, \
+             patch.object(dataset_service.author_repository, 'create') as mock_create_author, \
+             patch.object(dataset_service.repository, 'create') as mock_create_dataset, \
+             patch.object(dataset_service.repository.session, 'commit'):
 
-    with patch.object(dataset_service.dsmetadata_repository, 'create') as mock_create_dsmetadata, \
-         patch.object(dataset_service.author_repository, 'create') as mock_create_author, \
-         patch.object(dataset_service.repository, 'create') as mock_create_dataset, \
-         patch.object(dataset_service.repository.session, 'commit'):
+            mock_dsmetadata = MagicMock(spec=DSMetaData)
+            mock_dsmetadata.id = 1
+            mock_create_dsmetadata.return_value = mock_dsmetadata
 
-        mock_dsmetadata = MagicMock(spec=DSMetaData)
-        mock_dsmetadata.id = 1
-        mock_create_dsmetadata.return_value = mock_dsmetadata
+            result = dataset_service.create_from_form(form, mock_user)
 
-        result = dataset_service.create_from_form(form, mock_user)
-
-        mock_create_dsmetadata.assert_called_once_with(**form.get_dsmetadata())
-        mock_create_author.assert_called_with(
-            commit=False, ds_meta_data_id=mock_dsmetadata.id, **form.get_authors()[0]
-        )
-        assert result == mock_create_dataset.return_value
+            mock_create_dsmetadata.assert_called_once_with(**form.get_dsmetadata())
+            mock_create_author.assert_called_with(
+                commit=False, ds_meta_data_id=mock_dsmetadata.id, **form.get_authors()[0]
+            )
+            assert result == mock_create_dataset.return_value
 
 
 def test_get_synchronized(dataset_service):
-    with patch.object(dataset_service.repository, 'get_synchronized') as mock_get_synchronized:
-        mock_dataset = MagicMock(spec=DataSet)
-        mock_get_synchronized.return_value = mock_dataset
+    app = create_app()
+    with app.app_context():
+        with patch.object(dataset_service.repository, 'get_synchronized') as mock_get_synchronized:
+            mock_dataset = MagicMock(spec=DataSet)
+            mock_get_synchronized.return_value = mock_dataset
 
-        user_id = 1
-        result = dataset_service.get_synchronized(user_id)
+            user_id = 1
+            result = dataset_service.get_synchronized(user_id)
 
-        assert result == mock_dataset
-        mock_get_synchronized.assert_called_once_with(user_id)
+            assert result == mock_dataset
+            mock_get_synchronized.assert_called_once_with(user_id)
 
 
 def test_get_uvlhub_doi(dataset_service, mock_dataset):
-    with patch("os.getenv", return_value="testdomain.com"):
-        mock_dataset.ds_meta_data.dataset_doi = "10.1234/test-doi"
+    app = create_app()
+    with app.app_context():
+        with patch("os.getenv", return_value="testdomain.com"):
+            mock_dataset.ds_meta_data.dataset_doi = "10.1234/test-doi"
 
-        result = dataset_service.get_uvlhub_doi(mock_dataset)
+            result = dataset_service.get_uvlhub_doi(mock_dataset)
 
-        assert result == "http://testdomain.com/doi/10.1234/test-doi"
+            assert result == "http://testdomain.com/doi/10.1234/test-doi"
 
 
 def test_count_synchronized_datasets(dataset_service):
@@ -163,28 +173,34 @@ def test_get_human_readable_size(size_service):
 
 
 def test_get_unsynchronized(dataset_service):
-    with patch.object(dataset_service.repository, 'get_unsynchronized') as mock_get_unsynchronized:
-        mock_dataset = MagicMock(spec=DataSet)
-        mock_get_unsynchronized.return_value = mock_dataset
+    app = create_app()
+    with app.app_context():
+        with patch.object(dataset_service.repository, 'get_unsynchronized') as mock_get_unsynchronized:
+            mock_dataset = MagicMock(spec=DataSet)
+            mock_get_unsynchronized.return_value = [mock_dataset]
+            mock_dataset = MagicMock(spec=DataSet)
+            mock_get_unsynchronized.return_value = mock_dataset
 
-        user_id = 1
-        result = dataset_service.get_unsynchronized(user_id)
+            user_id = 1
+            result = dataset_service.get_unsynchronized(user_id)
 
-        assert result == mock_dataset
-        mock_get_unsynchronized.assert_called_once_with(user_id)
+            assert result == mock_dataset
+            mock_get_unsynchronized.assert_called_once_with(user_id)
 
 
 def test_get_unsynchronized_dataset(dataset_service):
-    with patch.object(dataset_service.repository, 'get_unsynchronized_dataset') as mock_get_unsynchronized_dataset:
-        mock_dataset = MagicMock(spec=DataSet)
-        mock_get_unsynchronized_dataset.return_value = mock_dataset
+    app = create_app()
+    with app.app_context():
+        with patch.object(dataset_service.repository, 'get_unsynchronized_dataset') as mock_get_unsynchronized_dataset:
+            mock_dataset = MagicMock(spec=DataSet)
+            mock_get_unsynchronized_dataset.return_value = mock_dataset
 
-        user_id = 1
-        dataset_id = 2
-        result = dataset_service.get_unsynchronized_dataset(user_id, dataset_id)
+            user_id = 1
+            dataset_id = 2
+            result = dataset_service.get_unsynchronized_dataset(user_id, dataset_id)
 
-        assert result == mock_dataset
-        mock_get_unsynchronized_dataset.assert_called_once_with(user_id, dataset_id)
+            assert result == mock_dataset
+            mock_get_unsynchronized_dataset.assert_called_once_with(user_id, dataset_id)
 
 
 def test_get_new_doi(doi_mapping_service):
@@ -196,3 +212,17 @@ def test_get_new_doi(doi_mapping_service):
 
         assert result == "10.5678/new-doi"
         mock_get_new_doi.assert_called_once_with(old_doi)
+
+
+def test_count_authors(dataset_service):
+    with patch.object(dataset_service, 'author_repository', MagicMock()) as author_repository_mock:
+        author_repository_mock.count.return_value = 15
+        result = dataset_service.count_authors()
+        assert result == 15
+
+
+def test_count_dsmetadata(dataset_service):
+    with patch.object(dataset_service, 'dsmetadata_repository', MagicMock()) as dsmetadata_repository_mock:
+        dsmetadata_repository_mock.count.return_value = 20
+        result = dataset_service.count_dsmetadata()
+        assert result == 20
