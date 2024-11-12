8ef92c0e3ca8bf7166d176679728982f2200e01b
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-11-12 23:44:08 +0100

    refactor: refactor AuthorRepository

--- app/modules/dataset/repositories.py
+++ app/modules/dataset/repositories.py
@@ -22,7 +22,7 @@ class AuthorRepository(BaseRepository):
     def __init__(self):
         super().__init__(Author)
 
-    def get_author_names_and_dataset_counts(self):
+    def get_author_names_and_dataset_counts():
         result = (
             Author.query
             .outerjoin(DSMetaData, Author.ds_meta_data_id == DSMetaData.id)
@@ -37,7 +37,8 @@ class AuthorRepository(BaseRepository):
         result = (
             Author.query
             .join(DSMetaData, Author.ds_meta_data_id == DSMetaData.id)
-            .outerjoin(DSViewRecord, DSMetaData.id == DSViewRecord.dataset_id)
+            .join(DataSet, DSMetaData.id == DataSet.ds_meta_data_id)
+            .outerjoin(DSViewRecord, DataSet.id == DSViewRecord.dataset_id)
             .with_entities(Author.name, func.count(DSViewRecord.id).label('view_count'))
             .group_by(Author.name)
             .order_by(func.count(DSViewRecord.id).desc())
