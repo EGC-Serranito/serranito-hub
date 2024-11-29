656b75dff25625ac88c11259a83fbe67f72fef18
IsaacSolisPadilla <isasolpad@alum.us.es>
2024-11-29 22:59:12 +0100

    fix: fix one dashboard error

--- app/modules/dashboard/repositories.py
+++ app/modules/dashboard/repositories.py
@@ -37,3 +37,4 @@ class DashboardAuthorRepository(BaseRepository):
             .all()
         )
         return result
+
--- app/modules/dashboard/routes.py
+++ app/modules/dashboard/routes.py
@@ -10,7 +10,7 @@ def index():
     author_names_dataset, dataset_counts = dashboardservice.get_all_author_names_and_dataset_counts()
     author_names_view, view_counts = dashboardservice.get_all_author_names_and_view_counts()
     dataset_names, total_sizes = dashboardservice.get_datasets_and_total_sizes()
-    
+    view_dates, view_counts_over_time = dashboardservice.get_views_over_time()
     return render_template(
         'dashboard/index.html',
         author_names_dataset=author_names_dataset,
@@ -18,5 +18,7 @@ def index():
         author_names_view=author_names_view,
         view_counts=view_counts,
         dataset_names=dataset_names,
-        total_sizes=total_sizes
+        total_sizes=total_sizes,
+        view_dates=view_dates,
+        view_counts_over_time=view_counts_over_time
     )
