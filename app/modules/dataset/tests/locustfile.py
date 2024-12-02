29610a0a871f05baddfd58cdfe3a6194de921df3
Juan Garcia Carballo <juangarciacarballo@gmail.com>
2024-12-02 15:16:38 +0100

    test: create locust test for rate dataset

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,16 +1,22 @@
 from locust import HttpUser, TaskSet, task
 from core.locust.common import get_csrf_token
 from core.environment.host import get_host_for_locust_testing
+import random
 
 
 class DatasetBehavior(TaskSet):
     def on_start(self):
         self.dataset()
 
-    @task
+    @task(2)
     def dataset(self):
         response = self.client.get("/dataset/upload")
         get_csrf_token(response)
+
+    @task(1)
+    def dataset_rate(self):
+        dataset_id = random.choice([1, 2, 3, 4])
+        self.client.post(f"/rate_dataset/{dataset_id}", data={"rate": 5})
 
 
 class DatasetUser(HttpUser):
