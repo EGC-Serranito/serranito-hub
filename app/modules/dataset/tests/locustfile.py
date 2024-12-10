bd7dff0d45db71a970d602d08986f5a35a055c40
Lucas Manuel Herencia Solís <lmherencia2003@gmail.com>
2024-12-10 22:54:43 +0100

    test: create locust test from update dataset and save edit uvl

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,6 +1,7 @@
 from locust import HttpUser, TaskSet, task
-from core.locust.common import get_csrf_token
+# from core.locust.common import get_csrf_token
 from core.environment.host import get_host_for_locust_testing
+from core.locust.common import get_csrf_token
 
 
 class DatasetBehavior(TaskSet):
@@ -19,8 +20,29 @@ class DatasetBehavior(TaskSet):
             self.client.post(f"/rate_dataset/{dataset_id}", data={"rate": (i*2) % 5})
 
 
+class UploadEditFileBehavior(TaskSet):
+    def on_start(self):
+        self.upload_edit_file()
+
+    @task
+    def upload_edit_file(self):
+        self.client.post("/dataset/1/upload/files", data={
+            "file_id": 1,
+            "content": "contenido "
+        })
+
+
+class UpdateDatasetBehavior(TaskSet):
+    def on_start(self):
+        self.update_dataset()
+
+    @task
+    def update_dataset(self):
+        self.client.get("/dataset/update/1")
+
+
 class DatasetUser(HttpUser):
-    tasks = [DatasetBehavior]
+    tasks = [DatasetBehavior, UploadEditFileBehavior, UpdateDatasetBehavior]
     min_wait = 5000
     max_wait = 9000
     host = get_host_for_locust_testing()
