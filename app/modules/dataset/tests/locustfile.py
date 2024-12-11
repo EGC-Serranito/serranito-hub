d9bc87488b6dbefcad1da28a7d5e9086300f4ddb
Lucas Manuel Herencia Solís <lmherencia2003@gmail.com>
2024-12-11 16:48:32 +0100

    chore: merge lobster functions into a single class

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,4 +1,5 @@
 from locust import HttpUser, TaskSet, task
+
 # from core.locust.common import get_csrf_token
 from core.environment.host import get_host_for_locust_testing
 from core.locust.common import get_csrf_token
@@ -7,6 +8,8 @@ from core.locust.common import get_csrf_token
 class DatasetBehavior(TaskSet):
     def on_start(self):
         self.dataset()
+        self.upload_edit_file()
+        self.update_dataset()
 
     @task(2)
     def dataset(self):
@@ -17,24 +20,13 @@ class DatasetBehavior(TaskSet):
     def dataset_rate(self):
         for i in range(4):
             dataset_id = i + 1
-            self.client.post(f"/rate_dataset/{dataset_id}", data={"rate": (i*2) % 5})
-
-
-class UploadEditFileBehavior(TaskSet):
-    def on_start(self):
-        self.upload_edit_file()
+            self.client.post(f"/rate_dataset/{dataset_id}", data={"rate": (i * 2) % 5})
 
     @task
     def upload_edit_file(self):
-        self.client.post("/dataset/1/upload/files", data={
-            "file_id": 1,
-            "content": "contenido"
-        })
-
-
-class UpdateDatasetBehavior(TaskSet):
-    def on_start(self):
-        self.update_dataset()
+        self.client.post(
+            "/dataset/1/upload/files", data={"file_id": 1, "content": "contenido"}
+        )
 
     @task
     def update_dataset(self):
@@ -42,7 +34,7 @@ class UpdateDatasetBehavior(TaskSet):
 
 
 class DatasetUser(HttpUser):
-    tasks = [DatasetBehavior, UploadEditFileBehavior, UpdateDatasetBehavior]
+    tasks = [DatasetBehavior]
     min_wait = 5000
     max_wait = 9000
     host = get_host_for_locust_testing()
