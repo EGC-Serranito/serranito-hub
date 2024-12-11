392a45b15354d7baaf318da8ea9f216a63a1ab0a
Dangalcan <megamagolas@gmail.com>
2024-12-11 16:47:52 +0100

    feat: update rosemary command in order to populate external mysql database

--- rosemary/cli.py
+++ rosemary/cli.py
@@ -21,7 +21,7 @@ from rosemary.commands.env import env
 from rosemary.commands.test import test
 from rosemary.commands.run_app import run_app
 from rosemary.commands.coverage_all import coverage_all
-from rosemary.commands.populate_dev_db import populate_filess_db
+from rosemary.commands.populate_dev_db import populate_external_db
 
 
 class RosemaryCLI(click.Group):
@@ -63,7 +63,7 @@ cli.add_command(selenium)
 cli.add_command(module_list)
 cli.add_command(run_app)
 cli.add_command(coverage_all)
-cli.add_command(populate_filess_db)
+cli.add_command(populate_external_db)
 
 
 if __name__ == '__main__':
