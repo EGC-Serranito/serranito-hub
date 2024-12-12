d542d254ac9702c614bb6f568c2562b36fc9b661
Marina Calero López <73230580+marcallop7@users.noreply.github.com>
2024-12-12 17:36:57 +0100

    fix: solve error

--- app/modules/profile/tests/test_selenium_profile_user.py
+++ app/modules/profile/tests/test_selenium_profile_user.py
@@ -3,7 +3,7 @@ from core.selenium.common import initialize_driver
 
 
 class TestSeleniumprofileuser:
-    def setup_method(self): 
+    def setup_method(self):
         self.driver = initialize_driver()
         self.driver.implicitly_wait(10)
 
@@ -33,13 +33,15 @@ class TestSeleniumprofileuser:
         self.driver.find_element(By.CSS_SELECTOR, ".row:nth-child(4) a").click()
 
     def test_selenium_view_user_profile_log_out(self):
-      self.driver.get("http://127.0.0.1:5000/")
-      self.driver.set_window_size(1702, 1073)
-      self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
-      self.driver.find_element(By.LINK_TEXT, "Doe, Jane").click()
-      self.driver.find_element(By.CSS_SELECTOR, ".row:nth-child(2) > .col-md-6 > .mb-3").click()
-      self.driver.find_element(By.ID, "email").click()
-      self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
-      self.driver.find_element(By.ID, "password").click()
-      self.driver.find_element(By.ID, "password").send_keys("1234")
-      self.driver.find_element(By.ID, "submit").click()
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.set_window_size(1702, 1073)
+        self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
+        self.driver.find_element(By.LINK_TEXT, "Doe, Jane").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, ".row:nth-child(2) > .col-md-6 > .mb-3"
+        ).click()
+        self.driver.find_element(By.ID, "email").click()
+        self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").send_keys("1234")
+        self.driver.find_element(By.ID, "submit").click()
