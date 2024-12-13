3d7e780e02fed6bd562e8e33841e848a37061964
Ángel García Escudero <angelgares6424@gmail.com>
2024-12-13 01:22:49 +0100

    feat: implement file upload endpoint with UVL validation

--- app/modules/hubfile/routes.py
+++ app/modules/hubfile/routes.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timezone
 import os
 import uuid
+from app.modules.flamapy.services import FlamapyService
 from flask import current_app, jsonify, make_response, request, send_from_directory
 from flask_login import current_user
 from app.modules.hubfile import hubfile_bp
 from app.modules.hubfile.models import HubfileDownloadRecord, HubfileViewRecord
 from app.modules.hubfile.services import HubfileDownloadRecordService, HubfileService
+from flask_login import login_required
 
 from app import db
+
+flamapy_service = FlamapyService()
 
 
 @hubfile_bp.route("/file/download/<int:file_id>", methods=["GET"])
@@ -47,6 +51,45 @@ def download_file(file_id):
     resp.set_cookie("file_download_cookie", user_cookie)
 
     return resp
+
+@hubfile_bp.route("/hubfile/upload", methods=["POST"])
+@login_required
+def upload_file():
+    file = request.files["file"]
+    temp_folder = current_user.temp_folder()
+
+    if not file or not file.filename.endswith(".uvl"):
+        return jsonify({"message": "No valid file"}), 400
+
+    # Crear carpeta temporal si no existe
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+
+    # Ruta temporal para el archivo
+    temp_file_path = os.path.join(temp_folder, file.filename)
+
+    # Guardar temporalmente el archivo
+    try:
+        file.save(temp_file_path)
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+    # Llamar directamente a `check_uvl` pasando la ruta temporal del archivo
+    validation_result, status_code = flamapy_service.check_uvl(temp_file_path)
+
+    if status_code != 200:
+        # Si no es válido, eliminar el archivo temporal y retornar error
+        os.remove(temp_file_path)
+        return jsonify(validation_result), status_code
+
+    # Si el archivo es válido, guardarlo permanentemente
+    new_filename = file.filename
+    return jsonify(
+        {
+            "message": "UVL uploaded and validated successfully",
+            "filename": new_filename,
+        }
+    ), 200
 
 
 @hubfile_bp.route('/file/view/<int:file_id>', methods=['GET'])
